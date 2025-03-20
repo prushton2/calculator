9dd288200e13690127561e99adc8452ff156ca36
prushton2 <peterrushton418@gmail.com>
2025-03-19 22:29:40 -0400

    cleaned up old code

--- main.c
+++ main.c
@@ -4,45 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// void printTokenType(TokenType tokentype) {
-//     switch(tokentype) {
-//         case 0:
-//             printf("PLUS");
-//             break;
-//         case 1:
-//             printf("MINUS");
-//             break;
-//         case 2:
-//             printf("MULTIPLY");
-//             break;
-//         case 3:
-//             printf("DIVIDE");
-//             break;
-//         case 4:
-//             printf("EXPONENT");
-//             break;
-//         case 5:
-//             printf("LPAREN");
-//             break;
-//         case 6:
-//             printf("RPAREN");
-//             break;
-//         case 7:
-//             printf("NUMBER");
-//             break;
-//     }
-
-// }
-
-
 int main() {
     Token* tokens[64] = {};
     Token** tokenPointer = tokens;
 
     char input[] = "(22-12)^(400/200)\0";
     char* inputPointer = input;
-
-    // Token tokens[64] = {};
 
     for(int i = 0; i < 64; i++) {
         Token* t = parseNextToken(&inputPointer);
@@ -58,10 +25,11 @@ int main() {
 
     traverse(node);
 
-    Literal* solved = (Literal*)solve(node);
+    Literal* solved = (Literal*)solve(node); //solve eats the node and frees it up
 
     printf("\n%d\n", solved->num);
 
+    free(solved);
     for(int i = 0; i < 64; i++) {
         if(tokens[i] != NULL)
             free(tokens[i]);
